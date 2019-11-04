44e285419ae61db1360fd917f414724055246b28
Hasiotis Nikos <hasiotis@gmail.com>
2019-11-04 21:51:27 +0200

    First try on gcp

---
+++ zbuilder/dns/gcp.py
@@ -0,0 +1,68 @@
+import time
+import click
+
+from google.cloud import dns
+
+DNS_TTL = 60 * 60 # 1 hour
+
+
+class dnsProvider(object):
+
+    def __init__(self, state):
+        self.state = state
+        self.dns = dns.Client(project="zbuilder-devel")
+
+    def _getZoneInfo(self, hostname):
+        zone = hostname.partition('.')[2]
+        host = hostname.partition('.')[0]
+        zones = self.dns.list_zones()
+        mzone = None
+        for z in zones:
+            if z.dns_name == zone + '.':
+                mzone = z
+                break
+
+        return(zone, host, mzone)
+
+
+    def update(self, ips):
+        for hostname, ip in ips.items():
+            (zone, host, mzone) = self._getZoneInfo(hostname)
+
+            try:
+                changes = mzone.changes()
+                changes.delete_record_set(mzone.resource_record_set(hostname + '.', 'A', DNS_TTL, [ip]))
+                changes.create()
+                while changes.status != 'done':
+                    time.sleep(5)
+                    changes.reload()
+                click.echo("  - Updating record [{}] with ip [{}]".format(hostname, ip))
+            except Exception as e:
+                click.echo("  - Creating record [{}] with ip [{}]".format(hostname, ip))
+
+            changes = mzone.changes()
+            changes.add_record_set(mzone.resource_record_set(hostname + '.', 'A', DNS_TTL, [ip]))
+            changes.create()
+            while changes.status != 'done':
+                time.sleep(5)
+                changes.reload()
+
+    def remove(self, hosts):
+        for h in hosts:
+            (zone, host, mzone) = self._getZoneInfo(h)
+            records = mzone.list_resource_record_sets()
+            found = False
+            for r in records:
+                if r.name == h + '.' and r.record_type in ['A', 'CNAME']:
+                    found = True
+                    click.echo("  - Removing record [{}]".format(h))
+                    changes = mzone.changes()
+                    changes.delete_record_set(mzone.resource_record_set(r.name, r.record_type, r.ttl, r.rrdatas))
+                    changes.create()
+                    while changes.status != 'done':
+                        time.sleep(5)
+                        changes.reload()
+                    break
+
+            if not found:
+                click.echo("  - No such record [{}]".format(h))
---
+++ zbuilder/vm/gcp.py
@@ -0,0 +1,131 @@
+import click
+import googleapiclient.discovery
+
+
+class vmProvider(object):
+
+    def __init__(self, state, dns):
+        self.state = state
+        self.dns = dns
+        self.compute = googleapiclient.discovery.build('compute', 'v1')
+
+    def _getVMs(self, hosts):
+        retValue = {}
+        for h, v in hosts.items():
+            if hosts[h]['enabled']:
+                result = self.compute.instances().list(
+                    project=v['project'],
+                    zone=v['zone'],
+                    filter="name=\"{}\"".format(h.replace('.', '-'))
+                ).execute()
+                items = result.get('items', [])
+                if items:
+                    retValue[h] = items[0]
+                else:
+                    retValue[h] = {'status': None}
+
+                    image_response = self.compute.images().getFromFamily(project=v['image']['project'], family=v['image']['family']).execute()
+                    source_disk_image = image_response['selfLink']
+
+                    retValue[h]['insert'] = self.compute.instances().insert(
+                        project=v['project'],
+                        zone=v['zone'],
+                        body={
+                            'name': h.replace('.', '-'),
+                            'machineType': "zones/{v[zone]}/machineTypes/{v[size]}".format(v=v),
+                            'disks': [
+                                {
+                                    'boot': True,
+                                    'autoDelete': True,
+                                    'initializeParams': {
+                                        'sourceImage': source_disk_image,
+                                    }
+                                }
+                            ],
+                            'networkInterfaces': [{
+                                'network': 'global/networks/default',
+                                'accessConfigs': [
+                                    {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+                                ]
+                            }],
+                        }
+                    )
+
+        return retValue
+
+    def build(self, hosts):
+        ips = {}
+        for h, v in self._getVMs(hosts).items():
+            if v['status'] is None:
+                click.echo("  - Creating host: {} ".format(h))
+                v['insert'].execute()
+                ips[h] = None
+            elif v['status'] == 'TERMINATED':
+                click.echo("  - Booting host: {} ".format(h))
+                self.compute.instances().start(project=hosts[h]['project'], zone=hosts[h]['zone'], instance=v['name']).execute()
+            elif v['status'] == "RUNNING":
+                click.echo("  - Already up host: {} ".format(h))
+            else:
+                click.echo("  - Status of host: {} is {}".format(h, v['status']))
+
+        # self.waitStatus(hosts, 'active')
+
+        for h, v in self._getVMs(hosts).items():
+            if h in ips:
+                ips[h] = v['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+
+        self.dns.update(ips)
+
+    def up(self, hosts):
+        for h, v in self._getVMs(hosts).items():
+            if v['status'] is None:
+                click.echo("  - No such host: {} ".format(h))
+            elif v['status'] == 'TERMINATED':
+                click.echo("  - Booting host: {} ".format(h))
+                self.compute.instances().start(project=hosts[h]['project'], zone=hosts[h]['zone'], instance=v['name']).execute()
+            elif v['status'] == "RUNNING":
+                click.echo("  - Already up host: {} ".format(h))
+            else:
+                click.echo("  - Status of host: {} is {}".format(h, v['status']))
+
+    def halt(self, hosts):
+        for h, v in self._getVMs(hosts).items():
+            if v['status'] is None:
+                click.echo("  - No such host: {} ".format(h))
+            if v['status'] == "RUNNING":
+                click.echo("  - Halting host: {} ".format(h))
+                self.compute.instances().stop(project=hosts[h]['project'], zone=hosts[h]['zone'], instance=v['name']).execute()
+            else:
+                click.echo("  - Status of host: {} is {}".format(h, v['status']))
+
+    def destroy(self, hosts):
+        for h, v in self._getVMs(hosts).items():
+            if v['status'] is not None:
+                click.echo("  - Destroying host: {} ".format(h))
+                self.compute.instances().delete(project=hosts[h]['project'], zone=hosts[h]['zone'], instance=v['name']).execute()
+            else:
+                click.echo("  - Host does not exists : {}".format(h))
+        self.dnsremove(hosts)
+
+    def dnsupdate(self, hosts):
+        ips = {}
+        for h, v in self._getVMs(hosts).items():
+            ips[h] = v['networkInterfaces'][0]['accessConfigs'][0]['natIP']
+
+        self.dns.update(ips)
+
+    def dnsremove(self, hosts):
+        ips = {}
+        for h in hosts:
+            if hosts[h]['enabled']:
+                ips[h] = None
+        self.dns.remove(ips)
+
+    def snapCreate(self, hosts):
+        pass
+
+    def snapRestore(self, hosts):
+        pass
+
+    def snapDelete(self, hosts):
+        pass
