f195e1730abba86680a2192f0748c3a6242f5f3d
Hasiotis Nikos <hasiotis@gmail.com>
2019-11-18 13:53:06 +0200

    Minor config setup fix

--- zbuilder/cfg.py
+++ zbuilder/cfg.py
@@ -5,17 +5,29 @@ from zbuilder.helpers import load_yaml, dump_yaml
 
 CONFIG_PATH = "~/.config/zbuilder/zbuilder.yaml"
 CONFIG_EMPTY = """# ZBuilder configuration
+main: {}
+
 providers: {}
 """
+
+
+def initConfig(fname):
+    Path(fname).parent.mkdir(parents=True, exist_ok=True)
+    Path(fname).touch()
+    Path(fname).open('w').write(CONFIG_EMPTY)
 
 
 def load(touch=False):
     fname = os.path.expanduser(CONFIG_PATH)
     if not os.path.exists(fname) and touch:
-        Path(fname).parent.mkdir(parents=True, exist_ok=True)
-        Path(fname).touch()
-        Path(fname).open('w').write(CONFIG_EMPTY)
-    return load_yaml(fname)
+        initConfig(fname)
+
+    retValue = load_yaml(fname)
+    if retValue is None:
+        initConfig(fname)
+        retValue = load_yaml(fname)
+
+    return retValue
 
 
 def view(cfg):
