f19e5a871b601587b111e454095bbe81d953688b
Nikos Chasiotis <hasiotis@gmail.com>
2020-06-05 11:24:20 +0300

    Support a list of dns servers for bind

--- zbuilder/dns/bind.py
+++ zbuilder/dns/bind.py
@@ -11,27 +11,33 @@ class dnsProvider(object):
             self.keyname = cfg['keyname']
             self.keysecret = cfg['keysecret']
             self.server = cfg['server']
+            if not isinstance(cfg['server'], list):
+                self.server = [cfg['server']]
             self.keyring = dns.tsigkeyring.from_text({self.keyname: self.keysecret})
 
+    def _dns_query(self, update):
+        for srv in self.server:
+            response = dns.query.tcp(update, srv)
+            if response.rcode():
+                err = dns.rcode.to_text(response.rcode())
+                click.echo("      Failed to update DNS record [{}] on server [{}]".format(err, srv))
+
     def update(self, host, zone, ip):
+        if ip is None:
+            click.echo("  - Skipping due to empty ip")
+            return
         fqdn = "{}.{}".format(host, zone)
         click.echo("  - Creating/Updating record [{}] with ip [{}]".format(fqdn, ip))
         update = dns.update.Update(zone, keyring=self.keyring)
         update.replace(host, 300, 'a', str(ip))
-        response = dns.query.tcp(update, self.server)
-        if response.rcode():
-            err = dns.rcode.to_text(response.rcode())
-            click.echo("      Failed to add DNS record: [%s]" % (err))
+        self._dns_query(update)
 
     def remove(self, host, zone):
         fqdn = "{}.{}".format(host, zone)
         click.echo("  - Removing record {}".format(fqdn))
         update = dns.update.Update(zone, keyring=self.keyring)
         update.delete(host)
-        response = dns.query.tcp(update, self.server)
-        if response.rcode():
-            err = dns.rcode.to_text(response.rcode())
-            click.echo("      Failed to remove DNS record: [%s]" % (err))
+        self._dns_query(update)
 
     def config(self):
         return "server: {}, keyname: {}".format(self.server, self.keyname)
